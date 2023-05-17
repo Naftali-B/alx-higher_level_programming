b98494911f1aa4c459313960e1b381995df237fe
Naftali-B <nbudamba@gmail.com>
2023-05-17 15:59:02 -0700

    SQL queries

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -2,7 +2,7 @@
 -- user_0d_2 should have only SELECT privilege in the database hbtn_0d_2
 
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_passwd';
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 GRANT USAGE ON *.* TO 'user_0d_2'@'localhost';
 GRANT SELECT ON `hbtn_0d_2`.* TO 'user_0d_2'@'localhost';
 FLUSH PRIVILEGES;
