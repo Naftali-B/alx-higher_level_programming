3c9096f3390c10a3eff2e3a7677b96fb50ed2afd
Naftali-B <nbudamba@gmail.com>
2023-06-12 18:43:35 -0700

    Javascript Warm-up

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const numOf = parseInt(process.argv[2], 10);
+if (Number.isNaN(numOf)) {
+console.log('Missing number of occurrences');
+}
+else {
+for (let i = 0; i < numOccurrences; i++) {
+console.log('C is fun');
+}
+}
