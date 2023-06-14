776aa87dd0892d999db32f27a8cfcb71ad49e190
Naftali-B <nbudamba@gmail.com>
2023-06-13 17:36:42 -0700

    Objects, Scopes and Closures

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 class Rectangle {
-  constructor(w, h) {
+  constructor (w, h) {
     if (w > 0 && h > 0) {
       this.width = w;
       this.height = h;
@@ -8,7 +8,8 @@ class Rectangle {
       return {};
     }
   }
-  print() {
+
+  print () {
     for (let i = 0; i < this.height; i++) {
       console.log('X'.repeat(this.width));
     }
