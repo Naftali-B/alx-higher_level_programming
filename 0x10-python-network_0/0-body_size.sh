3a947475c23693265056a3f33af60112fd82ebe2
Naftali-B <nbudamba@gmail.com>
2023-07-05 21:44:38 +0300

    Python - Network #0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,16 +1,3 @@
 #!/bin/bash
-
-# Check if URL argument is provided
-if [ $# -eq 0 ]; then
-	  echo "Usage: $0 <URL>"
-	    exit 1
-fi
-
-# Get the URL from command-line argument
-url=$1
-
-# Send a GET request using curl and output the size of the response body
-size=$(curl -s -w "%{size_download}" -o /dev/null "$url")
-
-# Display the size
-echo "Response size: $size bytes"
+# displays size of the body of the response
+curl -sI "$1" | grep -i Content-Length | awk '{print $2}'
