bbf321102db9c94e43e9cd8fc9f70d6ef4dd031c
Naftali-B <nbudamba@gmail.com>
2023-11-07 00:26:11 +0300

    inheritance

--- 0x0A-python-inheritance/5-base_geometry.py
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env/python3
 
-"""an empty class BaseGeometry """
+""" an empty class BaseGeometry """
 
 class BaseGeometry:
+    
     """
-    empty class BaseGeometry
+        empty class BaseGeometry
     """
     pass
