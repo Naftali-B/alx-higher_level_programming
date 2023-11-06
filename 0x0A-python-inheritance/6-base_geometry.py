7d5b0c37cc9720a9e56610a1ce9ccedb3602b2cc
Naftali-B <nbudamba@gmail.com>
2023-11-07 00:42:05 +0300

    inheritance

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+""" class BaseGeometry based on 5-base_geometry.py """
+
+
+class BaseGeometry:
+
+    """
+        class BaseGeometry
+        Methods:
+            area: raise exception 'area() is not implemented'
+    """
+
+    def area(self):
+
+        """
+            raises exception
+        """
+
+        raise Exception('area() is not implemented')
