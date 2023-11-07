52721242c990e016ca42f18d36d1d6c6f312681d
Naftali-B <nbudamba@gmail.com>
2023-11-07 18:40:13 +0300

    input-output

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" a function that reads a text file (UTF8)
+    and prints it to stdout: """
+
+
+def read_file(filename=""):
+    """
+        input: text file
+        uses with statement
+        so python closes the file automatically
+    """
+
+    with open(filename) as file:
+        for line in file:
+            print(line, end='')
