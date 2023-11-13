a0dbe34e3eaea872a2231158e907aa263648f499
Naftali-B <nbudamba@gmail.com>
2023-11-13 17:49:04 +0300

    almost a circle

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -12,27 +12,88 @@
 from models.rectangle import Rectangle
 
 class Square(Rectangle):
+    """
+    Square class, inherits from Rectangle.
+    """
+
     def __init__(self, size, x=0, y=0, id=None):
+        """
+        Initialize a Square instance.
+
+        Args:
+            size (int): Side length of the square.
+            x (int): x-coordinate of the square.
+            y (int): y-coordinate of the square.
+            id (int): Identifier of the square.
+
+        Returns:
+            None
+        """
+
         super().__init__(size, size, x, y, id)
 
     @property
     def size(self):
+        """
+        Get the size attribute.
+
+        Returns:
+            int: Size of the square.
+        """
+
         return self.width
 
     @size.setter
     def size(self, value):
+        """
+        Set the size attribute.
+
+        Args:
+            value (int): Size to set.
+
+        Returns:
+            None
+        """
+
         self.width = value
         self.height = value
 
     def __str__(self):
-        return "[Square] ({}) {}/{} - {}".format(
-            self.id, self.x, self.y, self.width
-        )
+        """
+        Return a string representation of the Square instance.
+
+        Returns:
+            str: Formatted string representation.
+        """
+
+        return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y, self.size)
+
+    def update(self, *args, **kwargs):
+        """
+        Update attributes of the Square instance.
+
+        Args:
+            *args: Variable length argument list (no-keyword arguments).
+            **kwargs: Arbitrary keyword arguments.
+
+        Returns:
+            None
+        """
+
+        if args:
+            attrs = ["id", "size", "x", "y"]
+            for i, value in enumerate(args):
+                setattr(self, attrs[i], value)
+        else:
+            for key, value in kwargs.items():
+                setattr(self, key, value)
 
     def to_dictionary(self):
-        return {
-            'id': self.id,
-            'size': self.size,
-            'x': self.x,
-            'y': self.y
-        }
+        """
+        Return a dictionary representation of the Square instance.
+
+        Returns:
+            dict: Dictionary containing the attributes of the Square.
+        """
+
+        return {"id": self.id, "size": self.size, "x": self.x, "y": self.y}
