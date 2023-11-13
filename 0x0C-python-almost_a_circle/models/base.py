9af9896c910d54450fb90399cfb1b4e2436437d6
Naftali-B <nbudamba@gmail.com>
2023-11-13 15:27:25 +0300

    almost a circle

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,4 +1,14 @@
-#!/usr/bin/python3
+    """
+    Class Base:
+    private class attribute __nb_objects = 0
+    class constructor: def __init__(self, id=None)::
+        if id is not None, assign the public instance attribute id
+        with this argument value - you can assume id is an integer
+        otherwise, increment __nb_objects
+        and assign the new value to the public instance attribute id
+        
+        This class will be the “base” of all other classes in this project.
+    """
 
 Class Base(self):
     __nb_objects = 0
