1f47ba4624430b2ae60201d2c35fd041e92b5eff
Naftali-B <nbudamba@gmail.com>
2024-02-21 00:07:23 +0300

    node

---
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+/* prints all characters of a Star Wars movie */
+
+const request = require('request');
+
+const args = process.argv.slice(2);
+const movieId = args[0];
+const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+
+request(url, (err, res, body) => {
+  if (err) console.log(err);
+  const movie = JSON.parse(body);
+  movie.characters.forEach((characterUrl) => {
+    request(characterUrl, (err, res, body) => {
+      if (err) console.log(err);
+      const character = JSON.parse(body);
+      console.log(character.name);
+    });
+  });
+});
+
